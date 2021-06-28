8b3713b08ade842721918c12fb902c9168978523
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-06-28 20:35:18 +0900

    Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() {
-  debugPaintSizeEnabled = false; // Set to true for visual layout
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  static final showCard = true; // Set to false to show Stack
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter layout demo',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Flutter layout demo'),
-            ),
-            body: ListView(
-              children: [
-                Image.asset(
-                  'cool.png',
-                  width: 600,
-                  height: 240,
-                  fit: BoxFit.cover,
-                ),
-                titleSection,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildButtonColumn(
-                        Theme.of(context).primaryColor, Icons.call, 'CALL'),
-                    _buildButtonColumn(
-                        Theme.of(context).primaryColor, Icons.near_me, 'ROUTE'),
-                    _buildButtonColumn(
-                        Theme.of(context).primaryColor, Icons.share, 'SHARE'),
-                    _buildButtonColumn(
-                        Theme.of(context).primaryColor, Icons.api, 'API'),
-                  ],
-                ),
-                textSection
-              ],
-            )));
+      home: HomeScreen(),
+    );
   }
+}
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Returning Data Demo'),
+      ),
+      // 다음 단계에서 SelectionButton 위젯을 만들 것입니다.
+      body: Center(child: SelectionButton()),
+    );
+  }
+}
+
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: Text('Pick an option, any option!'),
     );
   }
 
-  Widget textSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Text(
-      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-      'Alps. Situated 1,578 meters above sea level, it is one of the '
-      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-      'half-hour walk through pastures and pine forest, leads you to the '
-      'lake, which warms to 20 degrees Celsius in the summer. Activities '
-      'enjoyed here include rowing, and riding the summer toboggan run.',
-      softWrap: true,
-    ),
-  );
+  // SelectionScreen을 띄우고 navigator.pop으로부터 결과를 기다리는 메서드
+  _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push는 Future를 반환합니다. Future는 선택 창에서
+    // Navigator.pop이 호출된 이후 완료될 것입니다.
+    final result = await Navigator.push(
+      context,
+      // 다음 단계에서 SelectionScreen를 만들 것입니다.
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("$result"), duration: Duration(milliseconds: 300)),
+    );
+  }
+}
 
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // "Yep" 문자열과 함께 이전 화면으로 돌아갑니다...
+                  Navigator.pop(context, 'Yep!');
+                },
+                child: Text('Yep!'),
               ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // "Nope" 문자열과 함께 이전 화면으로 돌아갑니다.
+                  Navigator.pop(context, 'Nope!');
+                },
+                child: Text('Nope.'),
               ),
-            ],
-          ),
+            )
+          ],
         ),
-        /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
-      ],
-    ),
-  );
+      ),
+    );
+  }
 }
