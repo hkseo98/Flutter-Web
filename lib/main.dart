85b65ca09e39c2a7aa7107b12be121f403b5e957
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-07-04 12:07:16 +0900

    only front complete

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,20 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final String title;
   MyHomePage({required this.title});
+  final List<TodoList> todoMap = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MyBody(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 740) {
+            return MyBigBody(todoMap: todoMap);
+          } else {
+            return MySmallBody(todoMap: todoMap);
+          }
+        },
+      ),
       appBar: AppBar(
         title: Text(title),
       ),
@@ -64,7 +73,11 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class MyBody extends StatefulWidget {
+class MyBigBody extends StatefulWidget {
+  final List<TodoList> todoMap;
+
+  MyBigBody({required this.todoMap});
+
   @override
   _MyBodyState createState() => _MyBodyState();
 }
@@ -73,17 +86,17 @@ class TodoList {
   String name = '';
   bool completed = false;
   String registerdTime = '';
-  String dueDate = '';
+  DateTime? dueDate;
 
-  TodoList(
-      {required this.name, required this.registerdTime, required this.dueDate});
+  TodoList({required this.name, required this.registerdTime, this.dueDate});
 }
 
-class _MyBodyState extends State<MyBody> {
+class _MyBodyState extends State<MyBigBody> {
   TextEditingController _controller = TextEditingController();
-  List<TodoList> todoMap = [];
+  DateTime? _selectedTime;
 
-  onPress({name, registeredTime}) {
+  onPress({name, registeredTime, dueDate}) {
+    print(dueDate);
     if (name == '') {
       showDialog(
         context: context,
@@ -104,12 +117,13 @@ class _MyBodyState extends State<MyBody> {
       );
       return;
     }
-    TodoList todo = TodoList(name: name, registerdTime: registeredTime);
+    TodoList todo =
+        TodoList(name: name, registerdTime: registeredTime, dueDate: dueDate);
     setState(() {
-      todoMap.add(todo);
+      widget.todoMap.add(todo);
     });
-    print(todoMap);
-    _controller.text = '';
+    this._controller.text = '';
+    this._selectedTime = null;
   }
 
   @override
@@ -129,27 +143,177 @@ class _MyBodyState extends State<MyBody> {
                   child: TextFormField(
                     decoration: InputDecoration(hintText: 'Write Some Todo'),
                     autofocus: true,
-                    autocorrect: true,
                     controller: _controller,
                   ),
                 ),
               ),
             ),
-            ElevatedButton(
-                onPressed: () => {
-                      onPress(
-                          name: _controller.text,
-                          registeredTime: DateTime.now().toString())
-                    },
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text('submit'),
-                )),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 150),
+                child: ElevatedButton(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('picking D-Day'),
+                  ),
+                  onPressed: bringDatePicker,
+                ),
+              ),
+            ),
+            Container(
+              constraints: BoxConstraints(maxWidth: 100),
+              child: ElevatedButton(
+                  onPressed: () => {
+                        onPress(
+                            name: _controller.text,
+                            registeredTime: DateTime.now().toString(),
+                            dueDate: _selectedTime)
+                      },
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('submit'),
+                  )),
+            ),
           ],
         ),
-        ShowList(todoMap: todoMap),
+        ShowList(todoMap: widget.todoMap),
       ]),
     );
+  }
+
+  void bringDatePicker() {
+    Future<DateTime?> selectedDate = showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(DateTime.now().year),
+        lastDate: DateTime(DateTime.now().year + 20),
+        builder: (context, child) {
+          return Theme(data: ThemeData.dark(), child: child!);
+        });
+    selectedDate.then((dateTime) {
+      setState(() {
+        this._selectedTime = dateTime;
+      });
+    });
+  }
+}
+
+class MySmallBody extends StatefulWidget {
+  final List<TodoList> todoMap;
+  MySmallBody({required this.todoMap});
+  @override
+  _MySmallBodyState createState() => _MySmallBodyState();
+}
+
+class _MySmallBodyState extends State<MySmallBody> {
+  TextEditingController _controller = TextEditingController();
+  DateTime? _selectedTime;
+
+  onPress({name, registeredTime, dueDate}) {
+    print(dueDate);
+    if (name == '') {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: new Text("Warning"),
+            content: new Text("Write Something!!"),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('back'))
+            ],
+          );
+        },
+      );
+      return;
+    }
+    TodoList todo =
+        TodoList(name: name, registerdTime: registeredTime, dueDate: dueDate);
+    setState(() {
+      widget.todoMap.add(todo);
+    });
+    this._controller.text = '';
+    this._selectedTime = null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      child: Column(children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(12, 48, 12, 0),
+          child: Container(
+            width: 500,
+            height: 60,
+            child: Form(
+              child: TextFormField(
+                decoration: InputDecoration(hintText: 'Write Some Todo'),
+                autofocus: true,
+                controller: _controller,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 8, 0, 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 150),
+                  child: ElevatedButton(
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text('picking D-Day'),
+                    ),
+                    onPressed: bringDatePicker,
+                  ),
+                ),
+              ),
+              Container(
+                constraints: BoxConstraints(maxWidth: 100),
+                child: ElevatedButton(
+                    onPressed: () => {
+                          onPress(
+                              name: _controller.text,
+                              registeredTime: DateTime.now().toString(),
+                              dueDate: _selectedTime)
+                        },
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text('submit'),
+                    )),
+              ),
+            ],
+          ),
+        ),
+        ShowList(todoMap: widget.todoMap),
+      ]),
+    );
+  }
+
+  void bringDatePicker() {
+    Future<DateTime?> selectedDate = showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(DateTime.now().year),
+        lastDate: DateTime(DateTime.now().year + 20),
+        builder: (context, child) {
+          return Theme(data: ThemeData.dark(), child: child!);
+        });
+    selectedDate.then((dateTime) {
+      setState(() {
+        this._selectedTime = dateTime;
+      });
+    });
   }
 }
 
@@ -166,43 +330,57 @@ class _ShowListState extends State<ShowList> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        width: 900,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  'Work',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text('Completed',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                Text('Registered Time',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                Text('Delete', style: TextStyle(fontWeight: FontWeight.bold))
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(12),
-              child: Container(
-                height: 1,
-                constraints: BoxConstraints(minWidth: 300, maxWidth: 700),
-                color: Colors.black38,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: 900,
-                child: ListView(
-                  children: list().toList(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Center(
+                child: SizedBox(
+                  width: 100,
+                  child: Text(
+                    'Work',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            )
-          ],
-        ),
+              Center(
+                child: SizedBox(
+                  width: 100,
+                  child: Text('Completed',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                    width: 100,
+                    child: Text('Due Date',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+              ),
+              Center(
+                child: SizedBox(
+                    width: 100,
+                    child: Text('Delete',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(50, 12, 50, 12),
+            child: Container(
+              height: 1,
+              color: Colors.black38,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: ListView(
+                children: list().toList(),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -224,35 +402,39 @@ class _ShowListState extends State<ShowList> {
       return Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Container(
-              constraints: BoxConstraints(maxWidth: 100, minWidth: 100),
-              child: Text(element.name),
+          Center(
+            child: SizedBox(width: 100, child: Text(element.name)),
+          ),
+          Center(
+            child: SizedBox(
+              width: 50,
+              child: Checkbox(
+                value: element.completed,
+                onChanged: (bool? value) {
+                  setState(() {
+                    element.completed = value!;
+                  });
+                },
+                splashRadius: 15,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                checkColor: Colors.white,
+                fillColor: MaterialStateProperty.resolveWith(getColor),
+              ),
             ),
           ),
-          Checkbox(
-            value: element.completed,
-            onChanged: (bool? value) {
-              setState(() {
-                print(element.completed);
-                print(value);
-                element.completed = value!;
-              });
-            },
-            splashRadius: 15,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-            checkColor: Colors.white,
-            fillColor: MaterialStateProperty.resolveWith(getColor),
-          ),
-          Text(element.registerdTime),
-          ElevatedButton(
-              onPressed: () => setState(() {
-                    widget.todoMap.removeWhere(
-                        (e) => e.registerdTime == element.registerdTime);
-                  }),
-              child: Text('DELETE'))
+          showDueDate(element.dueDate),
+          Center(
+            child: SizedBox(
+              width: 100,
+              child: ElevatedButton(
+                  onPressed: () => setState(() {
+                        widget.todoMap.removeWhere(
+                            (e) => e.registerdTime == element.registerdTime);
+                      }),
+                  child: Text('DELETE')),
+            ),
+          )
         ],
       );
     });
@@ -264,5 +446,19 @@ class _ShowListState extends State<ShowList> {
     setState(() {
       value = !value!;
     });
+  }
+
+  showDueDate(DateTime? dueDate) {
+    if (dueDate == null) {
+      return SizedBox(
+        child: Center(child: Text('undecided')),
+        width: 100,
+      );
+    } else {
+      return SizedBox(
+        child: Center(child: Text(dueDate.toString().split(' ')[0])),
+        width: 100,
+      );
+    }
   }
 }
